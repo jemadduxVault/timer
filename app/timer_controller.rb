302e2cc78afbbbe49aecf9f69a2ac04b0c7cc9f1
John Maddux <jemaddux@gmail.com>
2014-02-15 17:23:31 -0500

    Timer example done

--- app/timer_controller.rb
+++ app/timer_controller.rb
@@ -1,10 +1,28 @@
 class TimerController < UIViewController
-  attr_reader :margin
+  attr_reader :timer
 
   def viewDidLoad
-    @margin = 20
-    create_label
-    create_margin
+    margin = 20
+
+    @title = UILabel.new
+    @title.font = UIFont,systemFontOfSize(40)
+
+
+    @state = UILabel.new
+    @state.font = UIFont.systemFontOfSize(30)
+    @state.text = ""
+    @state.textAlignment = UITextAlignmentCenter
+    @state.textColor = UIColor.whiteColor
+    @state.backgroundColor = UIColor.clearColor
+    @state.frame = [[margin, 200], [view.frame.size.width - margin * 2, 40]]
+    view.addSubview(@state)
+
+    @action = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @action.setTitle('Start', forState:UIControlStateNormal)
+    @action.setTitle('Stop', forState:UIControlStateSelected)
+    @action.addTarget(self, action:'actionTapped', forControlEvents:UIControlEventTouchUpInside)
+    @action.frame = [[margin, 260], [view.frame.size.width - margin * 2, 40]]
+    view.addSubview(@action)
   end
 
   def actionTapped
@@ -13,24 +31,12 @@ class TimerController < UIViewController
       @timer = nil
     else
       @duration = 0
-      @timer = NSTimer.scheduledTimerWithInterval(0.1, target: self, selector: ######)
+      @timer = NSTimer.scheduledTimerWithTimeInterval(0.1, target:self, selector:'timerFired', userInfo:nil, repeats:true)
     end
     @action.selected = !@action.selected?
   end
 
-  def timerFeed
+  def timerFired
     @state.text = "%.1f" % (@duration += 0.1)
-  end
-
-private
-  def create_label
-
-  end
-
-  def create_button
-    @action = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    @action.setTitle("Start", forState: UIControlStateNormal)
-    @action.setTitle("Stop",  forState: UiControlStateSelected)
-    @action.addTarget(self, )
   end
 end
